adf5fdb3828bb46854e89e973df1f1a0dc4d1c5a
Tanushree Tunstall <tanu@tunstall.in>
2025-01-15 10:28:11 +0000

    added --verbose to plot_venn

--- plot_venn.py
+++ plot_venn.py
@@ -25,44 +25,69 @@ def setup_arguments():
                         nargs = '+', 
                         required = True, 
                         help = 'Paths to the input files.')
-    parser.add_argument('-k', '--key_column', 
+    parser.add_argument('--key_column', 
                         required = True, 
                         help = 'Column name to extract keys for Venn diagram.')
-    parser.add_argument('-l', '--labels', 
-                        nargs = '+', 
-                        help = 'Labels for each set in the Venn diagram.')
+    parser.add_argument('--labels', 
+                        nargs = '*', 
+                        help = 'Optional labels for the sets in the Venn diagram. If not provided, labels will be generated automatically.')
     parser.add_argument('-t', '--plot_title', 
                         default = 'Venn Diagram of Sets', 
                         help = 'Title for the Venn diagram.')
     parser.add_argument('--output_file', 
                         default = 'output_venn.png', 
                         help = "Output file for the Venn diagram.")
+    parser.add_argument('--verbose', 
+                        action = 'store_true', 
+                        help = 'Enable verbose output.')
     return parser
 
 
 def main():
+    
     parser = setup_arguments()
     args = parser.parse_args()
     
+    start_time = time.time()
+    
+    if args.verbose:
+        print(f"\nReading files: '{args.files}'")
+
     # Ensure common key column is present in all files
-    if not common_columns(args.files, delimiters = 'auto', force = False):
+    print(f"\nChecking if '{args.key_column}' is present in files provided...")
+    if common_columns(args.files, delimiters = 'auto', force = False):
+        print(f"'{args.key_column}' is present in all files. Proceeding to generate plots...")
+    else:
         sys.stderr.write(f"Error: Specified key column '{args.key_column}' is not common across all files.\n")
         sys.exit(1)
+
+    if args.verbose:
+        print(f"\nStarting the process to read sets for plots.")
 
     # Extract keys from each file
     sets = [read_keys_from_file(file, args.key_column) for file in args.files]
     
+    if args.verbose:
+        print(f"\nSets read successfully. Elapsed time: '{time.time() - start_time:.2f}' seconds.")
+    
+    # Labels
     if args.labels and len(args.labels) != len(sets):
-        print("Error: The number of labels provided does not match the number of files.")
+        print("\nError: The number of labels provided does not match the number of files.")
         return
-    
-    # Create labels if not provided
-   # if not args.labels:
-    #    args.labels = [f"Set {i+1}" for i in range(len(sets))]
-    
+    elif not args.labels:
+        args.labels = [f"Set {i+1}" for i in range(len(sets))]
+        
+    if args.verbose:
+        print(f"\nGenerating Venn diagram with labels: {args.labels}")
+        
     # Generate and save Venn diagram
     plot_venn(*sets, labels = args.labels, plot_title = args.plot_title, output_file = args.output_file)
-    print(f"Venn diagram saved to {args.output_file}")
+    if args.output_file:
+        print(f"\nVenn diagram saved to {args.output_file}")
+    
+    if args.verbose:
+        print(f"\nVenn diagram successfully generated. Total elapsed time: {time() - start_time:.2f} seconds.")
+
 
 if __name__ == '__main__':
     main()
